6908a65d223e10295b73eb6b1a2bb69ed2791b69
Oliver Cordes <ocordes@astro.uni-bonn.de>
2024-03-19 15:59:02 +0100

    - test the gpt_image module

---
+++ mkgimage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+
+from gpt_image.disk import Disk
+from gpt_image.partition import Partition, PartitionType
+
+# create a new, 16 MB disk, size is in bytes
+disk = Disk("disk-image.raw")
+disk.create(16 * 1024 * 1024)
+
+# create a 2MB Linux partition named "boot"
+boot_part = Partition(
+        "boot", 
+        2 * 1024 * 1024, 
+        PartitionType.EFI_SYSTEM_PARTITION.value
+    )
+disk.table.partitions.add(boot_part)
+
+# create an 8MB Linux partition named "data"
+data_part = Partition(
+        "data", 
+        8 * 1024 * 1024, 
+        PartitionType.LINUX_FILE_SYSTEM.value
+    )
+disk.table.partitions.add(data_part)
+
+# commit the change to disk
+disk.commit()
+
+# dump the current GPT information:
+
+print(disk)
+
+
+if __name__ == '__main__':
+    print('Hello, World!')
