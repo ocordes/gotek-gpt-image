8d0c48d32b2e9d9fbbe433fb654704928e1a503d
Oliver Cordes <ocordes@astro.uni-bonn.de>
2024-03-21 06:41:53 +0100

    - change main program for tests

--- mkgimage.py
+++ mkgimage.py
@@ -3,38 +3,33 @@
 import os
 import sys
 
+"""
+Documentation (german):
+
+https://de.wikipedia.org/wiki/GUID_Partition_Table
+
+"""
 
 
 from gpt_image.disk import Disk
 from gpt_image.partition import Partition, PartitionType 
 
-# create a new, 16 MB disk, size is in bytes
-disk = Disk("disk-image.raw")
-disk.create(16 * 1024 * 1024)
-
-# create a 2MB Linux partition named "boot"
-boot_part = Partition(
-        "boot", 
-        2 * 1024 * 1024, 
-        PartitionType.EFI_SYSTEM_PARTITION.value
-    )
-disk.table.partitions.add(boot_part)
-
-# create an 8MB Linux partition named "data" .  
-data_part = Partition(
-        "data", 
-        8 * 1024 * 1024, 
-        PartitionType.LINUX_FILE_SYSTEM.value
-    )
-disk.table.partitions.add(data_part)
-
-# commit the change to disk
-disk.commit()
-
-# dump the current GPT information:
-
-print(disk)
-
 
 if __name__ == '__main__':
-    print('Hello, World!')
+    # create a new, 16 MB disk, size is in bytes
+    disk = Disk("disk-image.raw")
+    disk.create(16 * 1024 * 1024)
+
+    disk_part1 = Partition(
+        "FDISK001",
+        1024 * 1536,
+        PartitionType.BASIC_DATA_PARTITION.value
+    )
+    disk.table.partitions.add(disk_part1)
+
+    # commit the change to disk
+    disk.commit()
+
+    # dump the current GPT information:
+
+    print(disk)
